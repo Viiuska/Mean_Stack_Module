e41fb599b8dc0892422bf4980527296c1b2d857b
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-03-24 13:19:56 +0200

    modified addUser function

--- models/user.js
+++ models/user.js
@@ -32,7 +32,7 @@ module.exports.getUserByUsername = function(username, callback){
     User.findOne(query, callback)
 }
 
-module.exports.addUser = async function(newUser, callback){
+module.exports.addUser = function(newUser, callback){
     bcrypt.genSalt(10, (err, salt)=>{
         bcrypt.hash(newUser.password, salt, (err, hash)=>{
             if(err) throw err
