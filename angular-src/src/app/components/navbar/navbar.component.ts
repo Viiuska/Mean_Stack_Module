6ce08438feb3e484889e1e23bc16f90876bf1af8
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-03-30 16:40:37 +0300

    Register frontend done

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -13,30 +13,3 @@ export class NavbarComponent implements OnInit {
   }
 
 }
-
-
-/*
-<nav class="navbar navbar-default">
-  <div class="container">
-    <div class="nav-header">
-      <button type="button" class="nav-bar-toggle collapsed" data-toggle="collapsed">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">MEAN Auth App</a>
-    </div>
-    <div id="navbar" class="collapse navbar-collapse">
-      <ul class="nav navbar-nav navbar-left">
-        <li [routerLinkActivate]="['active']" [routerLinkActiveOptions] = "{exact:true}"><a  [routerLink]="['/']">Home</a></li>
-      </ul>
-
-      <ul class="nav navbar-nav navbar-right">
-        <li [routerLinkActivate]="['active']" [routerLinkActiveOptions] = "{exact:true}"><a  [routerLink]="['/login']">Home</a></li>
-        <li [routerLinkActivate]="['active']" [routerLinkActiveOptions] = "{exact:true}"><a  [routerLink]="['/register']">Home</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-*/
